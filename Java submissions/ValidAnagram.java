d6745eac293726b33ac8ce5176efeca6bd1e4e17
Khoo Jing Hong, Derrick <derrickkhoo2000@gmail.com>
2022-06-29 12:29:23 +0800

    Added ValidAnagram.java solution

---
+++ Java submissions/ValidAnagram.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] first = s.toCharArray();
+        char[] second = t.toCharArray();
+        Arrays.sort(first);
+        Arrays.sort(second);
+        if (first.length != second.length) {
+            return false;
+        }
+        else {
+            for (int i = 0; i < first.length; i++) {
+                if (first[i] != second[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
