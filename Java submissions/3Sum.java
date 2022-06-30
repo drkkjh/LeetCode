51a55921ee789f286f17c554a7db77c648487a27
Khoo Jing Hong, Derrick <derrickkhoo2000@gmail.com>
2022-06-30 23:48:31 +0800

    Added 3Sum.java solution

---
+++ Java submissions/3Sum.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    set.add(List.of(nums[i], nums[j++], nums[k--]));
+                }
+                else if (sum > 0) {
+                    k--;
+                }
+                else {
+                    j++;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
